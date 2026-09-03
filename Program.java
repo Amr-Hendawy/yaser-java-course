0aa98cf41aefc3b749a66f005ed4f3048ae2c5ef
Amr Hendawy <amr.fathy.ebraheem@gmail.com>
2026-09-03 21:17:46 +0300

    complete

--- Program.java
+++ Program.java
@@ -3,8 +3,14 @@
 // understanding the whole problem
 // Defining problem requirements
 // GUI - Console App
+// Class ==> is a blueprint / container that we put the code, methods, variables in it
 public class Program {
+    
+    public static void sayHello() {
+        System.out.print("Hello Yasser");
+    }
     public static void main(String[] args) {
+        sayHello();
         System.out.print("h");
     }
 }

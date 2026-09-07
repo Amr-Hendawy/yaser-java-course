5c2bb0238ea27768d5ccea1d99cf1d9c3c75ecbe
e0533065877-lang <e0533065877@gmail.com>
2026-09-07 23:53:10 +0300

    feat: add Precedence class for number parity check

---
+++ Precedence.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class Precedence {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+       int num;
+       System.out.print("Enter the number");
+       num = input.nextInt();
+       if(num % 2 == 0){
+       System.out.print("Even");
+
+
+       }
+       if(num % 2 == 1){
+        System.out.print("Odd");
+       }
+
+
+       
+       
+       
+       
+       
+       
+       
+        // int marks;
+        // System.out.println("Enter your marks");
+        // marks= input.nextInt();
+        // if(marks == 50){
+        // System.out.println("you passed the moudel");
+
+
+        }
+        
+
+        
+
+    //  System.out.println(2+3 *5);
+    //    System.out.print((2+3)*5);
+    //   int y = 10;
+    //   System.out.println(y = 10);
+    //   System.out.println(y == 10);
+    //   System.out.println(y != 10);
+    //   System.out.println(y > 10);
+    //   System.out.println(y < 10);
+      
+
+
+    }
+

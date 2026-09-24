b977cd2ee90586d046df1e193cf5d362bad5cf7a
Amr Hendawy <amr.fathy.ebraheem@gmail.com>
2026-09-24 15:59:23 +0300

    add new lines

--- session-7/NestedIf.java
+++ session-7/NestedIf.java
@@ -2,27 +2,31 @@
 import java.util.Scanner;
 
 public class NestedIf {
+
     public static void main(String[] args) {
-        Scanner var1 = new Scanner(System.in);
-        int var2 = var1.nextInt();
-//  1 2 3 4 5 6 7 8 9 ما لانهايه   5
-        if (var2 > 0) {
-            System.out.println("Positive Number");
-            if (var2 % 2 == 0) {
-                System.out.println("Even Number");
+        Scanner input = new Scanner(System.in);
+        int number; // Variable Declaration
+        System.out.println("Enter The number to test");
+        number = input.nextInt();
+        if (number > 0) {
+            if (number % 2 == 0) {
+                System.out.println("Positive Number +");
+                System.out.println("Even Number +");
             } else {
-                System.out.println("Odd Number");
+                System.out.println("Positive Number +");
+                System.out.println("Odd Number +");
             }
-        } else if (var2 < 0) {
-            System.out.println("Negative Number");
-            if (var2 % 2 == 0) {
-            System.out.println("Even Number");
+
+        } else if (number < 0) {
+                       if (number % 2 == 0) {
+                System.out.println("Negative Number -");
+                System.out.println("Even Number -");
             } else {
-            System.out.println("Odd Number");
+                System.out.println("Negative Number -");
+                System.out.println("Odd Number -");
             }
         } else {
-            System.out.print("Zero");
+            System.err.println("Zero");
         }
-
     }
 }
